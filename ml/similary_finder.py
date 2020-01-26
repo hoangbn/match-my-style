18f45570744cf77b1e884132c5322ce5990813fa
swarupsrini <swarup.srinivasan@mail.utoronto.ca>
2020-01-25 23:26:01 -0500

    Update similary_finder.py

--- ml/similary_finder.py
+++ ml/similary_finder.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from google.cloud import vision
 
 app = Flask(__name__)
 
@@ -11,11 +12,94 @@ cata_data = {
                 "pants": []
             }
 
-def 
+def create_product_set(project_id, location, product_set_id, product_set_display_name):
+    """Create a product set.
+    Args:
+        project_id: Id of the project.
+        location: A compute region name.
+        product_set_id: Id of the product set.
+        product_set_display_name: Display name of the product set.
+    """
+    client = vision.ProductSearchClient()
+
+    # A resource that represents Google Cloud Platform location.
+    location_path = client.location_path(
+        project=project_id, location=location)
+
+    # Create a product set with the product set specification in the region.
+    product_set = vision.types.ProductSet(
+            display_name=product_set_display_name)
+
+    # The response is the product set with `name` populated.
+    response = client.create_product_set(
+        parent=location_path,
+        product_set=product_set,
+        product_set_id=product_set_id)
+
+    # Display the product set information.
+    print('Product set name: {}'.format(response.name))
+
+
+def create_product(project_id, location, product_id, product_display_name, product_category):
+    """Create one product.
+    Args:
+        project_id: Id of the project.
+        location: A compute region name.
+        product_id: Id of the product.
+        product_display_name: Display name of the product.
+        product_category: Category of the product.
+    """
+    client = vision.ProductSearchClient()
+
+    # A resource that represents Google Cloud Platform location.
+    location_path = client.location_path(project=project_id, location=location)
+
+    # Create a product with the product specification in the region.
+    # Set product display name and product category.
+    product = vision.types.Product(
+        display_name=product_display_name,
+        product_category=product_category)
+
+    # The response is the product with the `name` field populated.
+    response = client.create_product(
+        parent=location_path,
+        product=product,
+        product_id=product_id)
+
+    # Display the product information.
+    print('Product name: {}'.format(response.name))
+
+
+def add_product_to_product_set(project_id, location, product_id, product_set_id):
+    """Add a product to a product set.
+    Args:
+        project_id: Id of the project.
+        location: A compute region name.
+        product_id: Id of the product.
+        product_set_id: Id of the product set.
+    """
+    client = vision.ProductSearchClient()
+
+    # Get the full path of the product set.
+    product_set_path = client.product_set_path(
+        project=project_id, location=location,
+        product_set=product_set_id)
+
+    # Get the full path of the product.
+    product_path = client.product_path(
+        project=project_id, location=location, product=product_id)
+
+    # Add the product to the product set.
+    client.add_product_to_product_set(
+        name=product_set_path, product=product_path)
+    print('Product added to product set.')
+
 
 @app.route("/getSimilar")
 def get_most_similar():
     threshold = request.args.get("percentage")
+    item = request.args.get("item")
+
 
 
 
