9a702ceb8e1e12827903433c5f4dcf60e65d33f5
Hemant Bhanot <hemant.bhanot01@gmail.com>
2020-01-26 08:58:32 -0500

    testing code

--- ml/similary_finder.py
+++ ml/similary_finder.py
@@ -156,7 +156,6 @@ def cleanProductSets(project_id, location, product_set_id):
     print('Product set deleted.')
 
 def cleanAll(project_id, location):
-
     # clean all prod_ids
     for list in prodSet_to_prods.values():
         for prod_ids in list:
@@ -165,8 +164,67 @@ def cleanAll(project_id, location):
     # clean prod sets
     for prod_sets in prodSet_to_prods:
         cleanProductSets(project_id, location, prod_sets)
-    
-def get_similar_products_uri(project_id, location, product_set_id, product_category, image_uri, filter):
+
+   
+# def get_similar_products_uri(project_id, location, product_set_id, product_category,image_uri, filter):
+#     """Search similar products to image.
+#     Args:
+#         project_id: Id of the project.
+#         location: A compute region name.
+#         product_set_id: Id of the product set.
+#         product_category: Category of the product.
+#         file_path: Local file path of the image to be searched.
+#         filter: Condition to be applied on the labels.
+#         Example for filter: (color = red OR color = blue) AND style = kids
+#         It will search on all products with the following labels:
+#         color:red AND style:kids
+#         color:blue AND style:kids
+#     """
+#     # product_search_client is needed only for its helper methods.
+#     product_search_client = vision.ProductSearchClient()
+#     image_annotator_client = vision.ImageAnnotatorClient()
+
+#     # Create annotate image request along with product search feature.
+#     image_source = vision.types.ImageSource(image_uri=image_uri)
+#     image = vision.types.Image(source=image_source)
+
+#     # product search specific parameters
+#     product_set_path = product_search_client.product_set_path(
+#         project=project_id, location=location,
+#         product_set=product_set_id)
+#     product_search_params = vision.types.ProductSearchParams(
+#         product_set=product_set_path,
+#         product_categories=[product_category],
+#         filter=filter)
+#     image_context = vision.types.ImageContext(
+#         product_search_params=product_search_params)
+
+#     # Search products similar to the image.
+#     response = image_annotator_client.product_search(
+#         image, image_context=image_context)
+
+#     index_time = response.product_search_results.index_time
+#     print('Product set index time:')
+#     print('  seconds: {}'.format(index_time.seconds))
+#     print('  nanos: {}\n'.format(index_time.nanos))
+
+#     results = response.product_search_results.results
+
+#     print('Search results:')
+#     for result in results:
+#         product = result.product
+
+#         print('Score(Confidence): {}'.format(result.score))
+#         print('Image name: {}'.format(result.image))
+
+#         print('Product name: {}'.format(product.name))
+#         print('Product display name: {}'.format(
+#             product.display_name))
+#         print('Product description: {}\n'.format(product.description))
+#         print('Product labels: {}\n'.format(product.product_labels))
+iid=3
+
+def get_similar_products_file(project_id, location, product_set_id, product_category,file_path, filter):
     """Search similar products to image.
     Args:
         project_id: Id of the project.
@@ -184,9 +242,12 @@ def get_similar_products_uri(project_id, location, product_set_id, product_categ
     product_search_client = vision.ProductSearchClient()
     image_annotator_client = vision.ImageAnnotatorClient()
 
+    # Read the image as a stream of bytes.
+    with open(file_path, 'rb') as image_file:
+        content = image_file.read()
+
     # Create annotate image request along with product search feature.
-    image_source = vision.types.ImageSource(image_uri=image_uri)
-    image = vision.types.Image(source=image_source)
+    image = vision.types.Image(content=content)
 
     # product search specific parameters
     product_set_path = product_search_client.product_set_path(
@@ -211,6 +272,7 @@ def get_similar_products_uri(project_id, location, product_set_id, product_categ
     results = response.product_search_results.results
 
     print('Search results:')
+    print(results)
     for result in results:
         product = result.product
 
@@ -222,9 +284,6 @@ def get_similar_products_uri(project_id, location, product_set_id, product_categ
             product.display_name))
         print('Product description: {}\n'.format(product.description))
         print('Product labels: {}\n'.format(product.product_labels))
-    #return result
-
-iid=3
 
 @app.route("/getSimilar")
 def get_most_similar():
@@ -257,15 +316,15 @@ def get_most_similar():
     try:
         create_product_set(PROJECT_ID, LOCATION, 'dd', 'shirts')
         create_product_set(PROJECT_ID, LOCATION, 'dd1', 'pants')
-        create_product(PROJECT_ID, LOCATION, 'del4', 'shirt1', 'apparel-v2')
-        create_product(PROJECT_ID, LOCATION, 'del5', 'pant1', 'apparel-v2')    
+        create_product(PROJECT_ID, LOCATION, 'del4', 'shirt1', CATEGORY)
+        create_product(PROJECT_ID, LOCATION, 'del5', 'pant1', CATEGORY)    
         add_product_to_product_set(PROJECT_ID, LOCATION, 'del4', 'dd')
         add_product_to_product_set(PROJECT_ID, LOCATION, 'del5', 'dd1')
         create_reference_image(PROJECT_ID, LOCATION, 'del4', 'bruh1', 'gs://matchmystyle-vcm/shirts/22756272_SearchResults.jpg')
         create_reference_image(PROJECT_ID, LOCATION, 'del4', 'bruh2', 'gs://matchmystyle-vcm/shirts/KIC_125-9156-0996-201_prod1 (1).jpg')
         create_reference_image(PROJECT_ID, LOCATION, 'del5', 'bruh3', 'gs://matchmystyle-vcm/pants/hmgoepprod.jfif')
         create_reference_image(PROJECT_ID, LOCATION, 'del5', 'bruh4', 'gs://matchmystyle-vcm/pants/hmgoepprod (1).jfif')
-        get_similar_products_uri(PROJECT_ID, LOCATION, 'dd', CATEGORY, 'gs://matchmystyle-vcm/shirts/22756272_SearchResults.jpg', 'True')
+        get_similar_products_file(PROJECT_ID, LOCATION, 'dd', CATEGORY, 'C:\\Users\\heman\\OneDrive\\Desktop\\Hemant\\Hackathons\\DeltaHacks2020\\MatchMyStyle\\MatchMyStyle\\demo imges\\sick1\\sick.jpg', None)
     except Exception as e:
         print(e)
         cleanAll(PROJECT_ID, LOCATION)
