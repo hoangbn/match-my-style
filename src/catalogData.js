1a6d750a7503ab38f0a153a363ca32a127e13a87
russellgoldman <russellgoldman1@gmail.com>
2020-01-26 09:28:48 -0500

    Update

--- src/catalogData.js
+++ src/catalogData.js
@@ -38,14 +38,19 @@ export const catalogData = {
             brand: 'H&M'
         },
         {
-            src: 'https://lp2.hm.com/hmgoepprod?set=source[/aa/30/aa30202e0074e5adfa8dc6e7f127c63c78f56857.jpg],origin[dam],category[men_trousers_trousers_skinny_all],type[DESCRIPTIVESTILLLIFE],hmver[1]&call=url[file:/product/main]',
-            name: 'Twill Pants Skinny Fit',
-            brand: 'H&M'
-        },
-        {
             src: 'https://imagena1.lacoste.com/dw/image/v2/AAUP_PRD/on/demandware.static/-/Sites-master/default/dw766f01fc/HH9553_PQ8_24.jpg?imwidth=767&impolicy=product',
             name: 'Slim Fit Stretch Gabardine Chino Pants',
             brand: 'Lacoste'
+        },
+        {
+            src: 'https://www.rlmedia.io/is/image/PoloGSI/s7-1350026_lifestyle?$rl_df_pdp_5_7_lif$',
+            name: 'Stretch Slim Fit Chino Pant',
+            brand: 'Ralph Lauren'
+        },
+        {
+            src: 'https://www.rlmedia.io/is/image/PoloGSI/s7-1356471_lifestyle?$rl_df_pdp_5_7_lif$',
+            name: 'Slim Fit Linen-Blend Pant',
+            brand: 'Ralph Lauren'
         },
         {
             src: 'https://imagena1.lacoste.com/dw/image/v2/AAUP_PRD/on/demandware.static/-/Sites-master/default/dw00e2fe5d/HH5541_02S_24.jpg?imwidth=767&impolicy=product',
@@ -53,9 +58,9 @@ export const catalogData = {
             brand: 'Lacoste'
         },
         {
-            src: 'https://www.rlmedia.io/is/image/PoloGSI/s7-1350026_lifestyle?$rl_df_pdp_5_7_lif$',
-            name: 'Stretch Slim Fit Chino Pant',
-            brand: 'Lacoste'
+            src: 'https://lp2.hm.com/hmgoepprod?set=source[/aa/30/aa30202e0074e5adfa8dc6e7f127c63c78f56857.jpg],origin[dam],category[men_trousers_trousers_skinny_all],type[DESCRIPTIVESTILLLIFE],hmver[1]&call=url[file:/product/main]',
+            name: 'Twill Pants Skinny Fit',
+            brand: 'H&M'
         },
     ]
 }
