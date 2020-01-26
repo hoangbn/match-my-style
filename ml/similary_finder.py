2ffe6cbd0743348e8e2a7fd8bc85ac7470e25be2
swarupsrini <swarup.srinivasan@mail.utoronto.ca>
2020-01-26 00:34:56 -0500

    Update similary_finder.py

--- ml/similary_finder.py
+++ ml/similary_finder.py
@@ -124,7 +124,6 @@ def create_reference_image(project_id, location, product_id, reference_image_id,
     print('Reference image uri: {}'.format(image.uri))
 
 
-def get_similar_products_uri(project_id, location, product_set_id, product_category, image_uri, filter):
     """Search similar products to image.
     Args:
         project_id: Id of the project.
@@ -182,10 +181,82 @@ def get_similar_products_uri(project_id, location, product_set_id, product_categ
         print('Product labels: {}\n'.format(product.product_labels))
 
 
+def get_similar_products_file(project_id, location, product_set_id, product_category, file_path, filter):
+    """Search similar products to image.
+    Args:
+        project_id: Id of the project.
+        location: A compute region name.
+        product_set_id: Id of the product set.
+        product_category: Category of the product.
+        file_path: Local file path of the image to be searched.
+        filter: Condition to be applied on the labels.
+        Example for filter: (color = red OR color = blue) AND style = kids
+        It will search on all products with the following labels:
+        color:red AND style:kids
+        color:blue AND style:kids
+    """
+    # product_search_client is needed only for its helper methods.
+    product_search_client = vision.ProductSearchClient()
+    image_annotator_client = vision.ImageAnnotatorClient()
+
+    # Read the image as a stream of bytes.
+    with open(file_path, 'rb') as image_file:
+        content = image_file.read()
+
+    # Create annotate image request along with product search feature.
+    image = vision.types.Image(content=content)
+
+    # product search specific parameters
+    product_set_path = product_search_client.product_set_path(
+        project=project_id, location=location,
+        product_set=product_set_id)
+    product_search_params = vision.types.ProductSearchParams(
+        product_set=product_set_path,
+        product_categories=[product_category],
+        filter=filter)
+    image_context = vision.types.ImageContext(
+        product_search_params=product_search_params)
+
+    # Search products similar to the image.
+    response = image_annotator_client.product_search(
+        image, image_context=image_context)
+
+    index_time = response.product_search_results.index_time
+    print('Product set index time:')
+    print('  seconds: {}'.format(index_time.seconds))
+    print('  nanos: {}\n'.format(index_time.nanos))
+
+    results = response.product_search_results.results
+
+    print('Search results:')
+    for result in results:
+        product = result.product
+
+        print('Score(Confidence): {}'.format(result.score))
+        print('Image name: {}'.format(result.image))
+
+        print('Product name: {}'.format(product.name))
+        print('Product display name: {}'.format(
+            product.display_name))
+        print('Product description: {}\n'.format(product.description))
+        print('Product labels: {}\n'.format(product.product_labels))
+
+iid=3
+
 @app.route("/getSimilar")
 def get_most_similar():
     threshold = request.args.get("percentage")
-    item = request.args.get("item")
+    data = request.json
+    create_product_set("matchmystyle", "us-east1", "1", "shirts")
+    create_product_set("matchmystyle", "us-east1", "2", "pants")
+    for shirt in data["shirts"]:
+        create_product("matchmystyle", "us-east1", iid, shirt["name"], shirt["brand"])
+        create_reference_image("matchmystyle", "us-east1", iid, )
+        iid+=1
+    for pant in data["pants"]:
+        create_product("matchmystyle", "us-east1", iid, pant["name"], pant["brand"])
+        iid+=1
+
 
     create_product_set('matchmystyle', 'us-east1', '1212', 'shirts')
     create_product('matchmystyle','us-east1', '12', 'shirt1', 'apparel-v2')
